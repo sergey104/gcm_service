393128f35903537a0d797c6133cc7fa002dddb86
Sergey <fil@localhost.localdomain>
2015-07-27 22:33:30 +0300

    mysql set

--- config/environments.rb
+++ config/environments.rb
@@ -5,32 +5,33 @@ require 'mysql2'
 # config/environments.rb
 # development
 configure :development do
-  db = URI.parse(ENV['DATABASE_URL'] || 'mysql2:///localhost/gcm')
   pool= 5
   username = root
   password =Remcolor777
   ActiveRecord::Base.establish_connection(
-          :adapter  => db.scheme == 'mysql2' ? 'mysql2' : db.scheme,
-          :host     => db.host,
-          :username => db.user,
-          :password => db.password,
-          :database => db.path[1..-1],
-          :encoding => 'utf8'
+          :adapter  => 'mysql2',
+          :host     => 'localhost',
+          :username => 'root',
+          :password => 'Remcolor777',
+          :database => 'gsm-development',
+          :encoding => 'utf8',
+          :socket   =>  '/var/lib/mysql/mysql.sock'
   )
 end
 
 # production
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+  db = URI.parse(ENV['DATABASE_URL'] || 'mysql2:///localhost/gcm')
   pool= 5
   username = root
-  password = Remcolor777
+  password =Remcolor777
   ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'mysql2' ? 'mysql2' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
+      :adapter  => 'mysql2',
+      :host     => 'localhost',
+      :username => 'root',
+      :password => 'Remcolor777',
+      :database => 'gsm',
+      :encoding => 'utf8',
+      :socket   =>  '/var/lib/mysql/mysql.sock'
   )
 end
--- db/migrate/20150611110729_create_mobile_user.rb
+++
@@ -1,4 +0,0 @@
-class CreateMobileUser < ActiveRecord::Migration
-  def change
-  end
-end
